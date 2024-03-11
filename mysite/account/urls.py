ada83b7504024e8b3892251d765259e2ec102725
121132 <1271194370@qq.com>
2024-03-11 16:07:27 +0800

    test

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import user, admin, account, chart,
+from .views import user, admin, account, chart
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
